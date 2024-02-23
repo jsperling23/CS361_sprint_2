4f73cff6a6b24c0f6d3d50b3af93fe1ffb363b0a
jsperling23 <95095370+jsperling23@users.noreply.github.com>
2024-02-22 17:30:09 -0800

    Update travelTest.py

--- Sprint_2/travelTest.py
+++ Sprint_2/travelTest.py
@@ -1,6 +1,14 @@
 import subprocess
+import json
 
 programPath = 'travelRequest.py'
 countryCode = 'EG'
 endPath = '/Users/joseph/Downloads/testing.txt'
+
+#Call the program
 subprocess.run(['python3', programPath, countryCode, endPath])
+
+#Open file and print to the console
+with open(endPath, 'r') as file:
+    data = json.load(file)
+    print(data)
