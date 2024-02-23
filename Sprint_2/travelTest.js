783ae59eb55ab596095cf3437aa7980b55c8d272
jsperling23 <95095370+jsperling23@users.noreply.github.com>
2024-02-22 17:29:32 -0800

    Update travelTest.js

--- Sprint_2/travelTest.js
+++ Sprint_2/travelTest.js
@@ -1,8 +1,14 @@
 const { spawnSync } = require('child_process');
+const fs = require('fs');
 
-const programPath = 'travelRequest.py'
-const countryCode = 'EG'
-const endPath = '/Users/joseph/Downloads/testing.txt'
+const programPath = 'travelRequest.py';
+const countryCode = 'EG';
+const endPath = '/Users/joseph/Downloads/testing.txt';
 
+//Call the proram
 spawnSync('python3', [programPath, countryCode, endPath]);
-    
+
+//Open text file and write to console
+const file = fs.readFileSync(endPath);
+const data = JSON.parse(file);
+console.log(data);
